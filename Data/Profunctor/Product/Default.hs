c91d41314ed5b5a0da1ad45c25c985647f65fc7b
Tom Ellis <tom-2013@purelyagile.com>
2013-12-18 09:36:48 +0000

    Add Default instance for 9 tuples

--- Data/Profunctor/Product/Default.hs
+++ Data/Profunctor/Product/Default.hs
@@ -58,6 +58,14 @@ instance (ProductProfunctor p, Default p a1 b1, Default p a2 b2,
 instance (ProductProfunctor p, Default p a1 b1, Default p a2 b2,
           Default p a3 b3, Default p a4 b4, Default p a5 b5,
           Default p a6 b6, Default p a7 b7, Default p a8 b8,
+          Default p a9 b9)
+         => Default p (a1, a2, a3, a4, a5, a6, a7, a8, a9)
+                      (b1, b2, b3, b4, b5, b6, b7, b8, b9) where
+  def = p9 (def, def, def, def, def, def, def, def, def)
+
+instance (ProductProfunctor p, Default p a1 b1, Default p a2 b2,
+          Default p a3 b3, Default p a4 b4, Default p a5 b5,
+          Default p a6 b6, Default p a7 b7, Default p a8 b8,
           Default p a9 b9, Default p a10 b10)
          => Default p (a1, a2, a3, a4, a5, a6, a7, a8,
                        a9, a10)
