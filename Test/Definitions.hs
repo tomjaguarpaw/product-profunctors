c3b2eeb870e7f36b5a94c00b1ff171a5327a401e
Jakub Ryška <ryskajakub@seznam.cz>
2015-12-20 18:14:33 +0100

    test the generation of default name adaptor

--- Test/Definitions.hs
+++ Test/Definitions.hs
@@ -8,7 +8,7 @@ module Definitions where
 -- because we want to ensure that no external names are required to be
 -- imported.
 
-import Data.Profunctor.Product.TH (makeAdaptorAndInstance)
+import Data.Profunctor.Product.TH (makeAdaptorAndInstance, makeAdaptorAndInstance')
 
 data Data2 a b = Data2 a b
 data Data3 a b c = Data3 a b c
@@ -16,7 +16,10 @@ data Data3 a b c = Data3 a b c
 data Record2 a b = Record2 { a2 :: a, b2 :: b }
 data Record3 a b c = Record3 { a3 :: a, b3 :: b, c3 :: c }
 
+data RecordDefaultName x y = RecordDefaultName { x :: x, y :: y }
+
 $(makeAdaptorAndInstance "pData2" ''Data2)
 $(makeAdaptorAndInstance "pData3" ''Data3)
 $(makeAdaptorAndInstance "pRecord2" ''Record2)
 $(makeAdaptorAndInstance "pRecord3" ''Record3)
+makeAdaptorAndInstance' ''RecordDefaultName
