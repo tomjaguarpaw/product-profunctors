ccdd3c0ead252f2dd6e27f88dc5c8a1acc5c2f02
Tom Ellis <tom-2013@purelyagile.com>
2016-02-07 09:42:43 +0000

    Comment that the adaptor is a generalization of a traverse

--- Data/Profunctor/Product/TH.hs
+++ Data/Profunctor/Product/TH.hs
@@ -38,6 +38,11 @@
 --         Foo (p a a') (p b b') (p c c') -> p (Foo a b c) (Foo a' b' c')
 -- @
 --
+-- This is a generalization of @Data.Traversable.Sequence@ in two
+-- different ways.  Firstly it works on datatypes with multiple type
+-- parameters.  Secondly it works on 'ProductProfunctor's, which are
+-- themselves a generalization of 'Applicative's.
+--
 -- If your type has only one field, for example
 --
 -- @
