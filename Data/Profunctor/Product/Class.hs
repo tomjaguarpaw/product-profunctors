3055f52763167e58d54efb47ee9ac0e2bfc2c20f
Tom Ellis <tom-2013@purelyagile.com>
2016-05-28 10:40:05 +0100

    Improve docs

--- Data/Profunctor/Product/Class.hs
+++ Data/Profunctor/Product/Class.hs
@@ -6,6 +6,11 @@ import Data.Profunctor (Profunctor)
 -- It has an "input", contravariant type parameter on the left as well
 -- as the usual 'Applicative' "output", covariant parameter on the
 -- right.
+--
+-- You will find it easier to see the similarity between
+-- 'ProductProfunctor' and 'Applicative' if you look at @purePP@,
+-- @***$@, and @****@, which correspond to @pure@, @<$>@, and @<*>@
+-- respectively.
 class Profunctor p => ProductProfunctor p where
   empty :: p () ()
   (***!) :: p a b -> p a' b' -> p (a, a') (b, b')
