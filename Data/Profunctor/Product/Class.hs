9c20b0050cb912629a16a8fea529062205592946
Tom Ellis <tom-2013@purelyagile.com>
2016-05-28 10:46:51 +0100

    How to make instances of ProductProfunctor

--- Data/Profunctor/Product/Class.hs
+++ Data/Profunctor/Product/Class.hs
@@ -12,6 +12,25 @@ import Data.Profunctor (Profunctor)
 -- 'ProductProfunctor' and 'Applicative' if you look at @purePP@,
 -- @***$@, and @****@, which correspond to @pure@, @<$>@, and @<*>@
 -- respectively.
+--
+-- It's easy to make instances of 'ProductProfunctor'.  Just make
+-- instances
+--
+-- @
+--  instance Profunctor MyProductProfunctor where
+--    ...
+--
+--  instance Applicative (MyProductProfunctor a) where
+--    ...
+-- @
+--
+-- and then write
+--
+-- @
+--  instance ProductProfunctor Writer where
+--    empty  = defaultEmpty
+--    (***!) = defaultProfunctorProduct
+-- @
 class Profunctor p => ProductProfunctor p where
   empty :: p () ()
   (***!) :: p a b -> p a' b' -> p (a, a') (b, b')
