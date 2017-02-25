e96ca9760bf894bea90b9dae7ccb7be637e07b7a
Tom Ellis <tom-2013@purelyagile.com>
2017-02-25 14:16:42 +0000

    Comment on Tuples cruft

--- Data/Profunctor/Product/Tuples.hs
+++ Data/Profunctor/Product/Tuples.hs
@@ -1,4 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
+
+-- | This is old cruft.  You should never use this and it will likely
+-- be deprecated in a future version.
+
 module Data.Profunctor.Product.Tuples where
 
 import Data.Profunctor.Product.Tuples.TH
