856d8fb04a792f50d0b803e484e00b1f52a10542
lyxia <lysxia@gmail.com>
2017-03-04 09:11:53 -0500

    Constraint synonyms for Default

--- Test/DefinitionsUndecidable.hs
+++ Test/DefinitionsUndecidable.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE UndecidableInstances  #-}
 
 module DefinitionsUndecidable where
@@ -15,18 +19,56 @@ module DefinitionsUndecidable where
 
 import GHC.Generics (Generic)
 import Data.Profunctor.Product (ProductProfunctor, SumProfunctor)
-import Data.Profunctor.Product.Default (Default)
+import Data.Profunctor.Product.Default
+  (Default, DefaultFields', DefaultConstraints, DefaultConstraints')
 
 data MonomorphicProduct = Product Int Bool            deriving Generic
 data MonomorphicSum     = A Int | B Bool              deriving Generic
 data MonomorphicBoth    = Both1 Char | Both2 Int Bool deriving Generic
+data PolyProduct a b c  = PolyProduct a b c           deriving Generic
 
-instance (ProductProfunctor p, Default p Bool Bool, Default p Int Int)
+instance (ProductProfunctor p, DefaultFields' p MonomorphicProduct)
          => Default p MonomorphicProduct MonomorphicProduct
 
-instance (SumProfunctor p, Default p Bool Bool, Default p Int Int)
+instance (SumProfunctor p, DefaultFields' p MonomorphicSum)
          => Default p MonomorphicSum MonomorphicSum
 
-instance (ProductProfunctor p, SumProfunctor p,
-          Default p Bool Bool, Default p Char Char, Default p Int Int)
+instance (DefaultConstraints' p MonomorphicBoth)
          => Default p MonomorphicBoth MonomorphicBoth
+
+instance
+  ( DefaultConstraints p
+      (PolyProduct a b c)
+      (PolyProduct a' b' c')
+  ) => Default p (PolyProduct a b c) (PolyProduct a' b' c')
+
+-- A constraint @c@.
+data Dict c where
+  Dict :: c => Dict c
+
+-- Entailment @c => d@.
+data c :- d where
+  Sub :: (c => Dict d) -> c :- d
+
+-- Equivalence of constraints.
+type c :<=>: d = (c :- d, d :- c)
+
+checkDFMonomorphicProduct
+  :: DefaultFields' p MonomorphicProduct :<=>:
+     (Default p Int Int, Default p Bool Bool)
+checkDFMonomorphicProduct = (Sub Dict, Sub Dict)
+
+checkDFMonomorphicSum
+  :: DefaultFields' p MonomorphicSum :<=>:
+     (Default p Int Int, Default p Bool Bool)
+checkDFMonomorphicSum = (Sub Dict, Sub Dict)
+
+checkDCMonomorphicBoth
+  :: DefaultConstraints' p MonomorphicBoth :<=>:
+     (ProductProfunctor p, SumProfunctor p, Default p Int Int, Default p Bool Bool, Default p Char Char)
+checkDCMonomorphicBoth = (Sub Dict, Sub Dict)
+
+checkDCPolyProduct
+  :: DefaultConstraints p (PolyProduct a b c) (PolyProduct a' b' c') :<=>:
+     (ProductProfunctor p, Default p a a', Default p b b', Default p c c')
+checkDCPolyProduct = (Sub Dict, Sub Dict)
