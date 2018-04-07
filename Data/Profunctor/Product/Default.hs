cf501d08f0cb1decf339c73580f4f15f35220ab8
Tom Ellis <tom-2013@purelyagile.com>
2018-04-07 10:14:46 +0100

    Haddock: Give an explanation for Default

--- Data/Profunctor/Product/Default.hs
+++ Data/Profunctor/Product/Default.hs
@@ -2,6 +2,44 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances,
              FlexibleContexts, PolyKinds, TemplateHaskell #-}
 
+-- | For some 'Data.Profunctor.Product.ProductProfunctor's @p@ and
+-- types @a@, @a'@ there is a unique most sensible value of @p a a'@.
+-- 'Default' exists to automatically generate that unique most
+-- sensible value for a product given unique most sensible values for
+-- the base types. If the unique most sensible values of type @p a
+-- a'@, @p b b'@ and @p c c'@ are
+--
+-- @
+-- sensible_a :: p a a'
+-- sensible_b :: p b b'
+-- sensible_c :: p c c'
+-- @
+--
+-- then the unique most sensible value of type @p (a, b, c) (a', b',
+-- c')@ is
+--
+-- @
+--'Data.Profunctor.Product.p3' (sensible_a, sebsible_b, sensible_c)
+--     :: p (a, b, c) (a', b', c')
+-- @
+--
+-- Therefore there is an instance
+--
+-- @
+-- instance
+--   ( 'Default' p a a'
+--   , 'Default' p b b'
+--   , 'Default' p c c'
+--   )
+-- => 'Default' p (a, b, c) (a', b', c')
+--     where 'def' = 'Data.Profunctor.Product.p3' ('def', 'def', 'def')
+-- @
+--
+-- which can be read as "if the unique most sensible values of types
+-- ... are ... then the unique most sensible value of the 3-tuple is
+-- given by composing them with 'p3'".  Naturally each different
+-- product type has a different composition function.
+
 module Data.Profunctor.Product.Default
   ( module Data.Profunctor.Product.Default
   , module Data.Profunctor.Product.Default.Class
