508281a4c822803e3cb5288d70077a93299c6e62
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-03-04 19:23:53 +0000

    Avoid splicing a quote

--- Data/Profunctor/Product/Internal/TH.hs
+++ Data/Profunctor/Product/Internal/TH.hs
@@ -180,7 +180,7 @@ instanceDefinition side tyName' numTyVars numConVars adaptorName' conName =
         tyName :: String -> Q Type
         tyName suffix = pure $ pArg' tyName' suffix numTyVars
 
-        instanceType = [t| $(conT ''Default) $p $pArg0 $pArg1 |]
+        instanceType = [t| Default $p $pArg0 $pArg1 |]
 
         defDefinition = FunD 'def [simpleClause defBody]
         defBody = NormalB(VarE adaptorName' `AppE` appEAll (ConE conName) defsN)
